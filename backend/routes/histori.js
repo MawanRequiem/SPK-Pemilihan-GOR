7ee0eeed69af023698a0377ff367a99e9a175b52
Dharmawan Surya Kusuma <112691285+MawanRequiem@users.noreply.github.com>
2025-07-04 09:57:31 +0000

    Delete backend directory

--- backend/routes/histori.js
+++
@@ -1,8 +0,0 @@
-// File: routes/history.js
-const express = require('express')
-const router = express.Router()
-const historyCtrl = require('../controllers/historyController')
-
-router.get('/:user_id', historyCtrl.getHistory)
-
-module.exports = router
--- backend/routes/rekomendasi.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const rekomendasiController = require('../controllers/rekomendasiController')
-
-router.post('/', rekomendasiController.prosesDIA)
-
-module.exports = router
